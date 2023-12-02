0d6c8192704884b95cb8f614677bb80e797a2694
joscha-gabriel-git <joschagabrielacm@gmail.com>
2023-12-02 08:42:43 +0700

    update mob ver

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  content: {
+    content: {
+      relative: true,
+      transform: (content) => content.replace(/taos:/g, ""),
+      files: ["./src/*.{html,js}"],
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("taos/plugin")],
+  safelist: ["!duration-[0ms]", "!delay-[0ms]", 'html.js :where([class*="taos:"]:not(.taos-init))'],
+};
